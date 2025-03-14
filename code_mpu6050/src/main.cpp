db36ea6d9117be2dc5b13e95536a6cd616d53a4c
isaacdutras <isaacdutra2017@gmail.com>
2025-03-14 10:29:12 -0400

    feat: amplia o número de sensores para 4

--- code_mpu6050/src/main.cpp
+++ code_mpu6050/src/main.cpp
@@ -5,175 +5,123 @@
 MPU6050 mpu7;
 MPU6050 mpu6;
 MPU6050 mpu5;
+MPU6050 mpu4;
 
-// Inicializa o Multiplexador com o endereço configurado
 void TCA9548A(uint8_t id)
 {
-    Wire.beginTransmission(0x70); // A0= LOW; A1= LOW; A2= LOW
+    Wire.beginTransmission(0x70);
     Wire.write(1 << id);
     Wire.endTransmission();
 }
 
-bool dmpReady = false;
-uint8_t mpuIntStatus;
-uint8_t devStatus;
-uint16_t packetSize;
-uint16_t fifoCount;
-uint8_t fifoBuffer[64];
-Quaternion q;
-VectorFloat gravity;
-float ypr[3];
-volatile bool mpuInterrupt = false;
+bool dmpReady[4] = {false};
+uint8_t mpuIntStatus[4];
+uint8_t devStatus[4];
+uint16_t packetSize[4];
+uint16_t fifoCount[4];
+uint8_t fifoBuffer[4][64];
+Quaternion q[4];
+VectorFloat gravity[4];
+float ypr[4][3];
+volatile bool mpuInterrupt[4] = {false};
 
-void dmpDataReady()
+void dmpDataReady(int index)
 {
-    mpuInterrupt = true;
+    mpuInterrupt[index] = true;
 }
 
-bool dmpReady6 = false;
-uint8_t mpuIntStatus6;
-uint8_t devStatus6;
-uint16_t packetSize6;
-uint16_t fifoCount6;
-uint8_t fifoBuffer6[64];
-Quaternion q6;
-VectorFloat gravity6;
-float ypr6[3];
-volatile bool mpuInterrupt6 = false;
-
-void dmpDataReady6()
+void initializeMPU(MPU6050 &mpu, uint8_t index, uint8_t channel)
 {
-    mpuInterrupt6 = true;
+    TCA9548A(channel);
+    delay(100);
+    mpu.initialize();
+    devStatus[index] = mpu.dmpInitialize();
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788);
+    if (devStatus[index] == 0)
+    {
+        Serial.print("MPU");
+        Serial.print(index + 3);
+        Serial.println(" pronto");
+        mpu.setDMPEnabled(true);
+        mpuIntStatus[index] = mpu.getIntStatus();
+        dmpReady[index] = true;
+        packetSize[index] = mpu.dmpGetFIFOPacketSize();
+    }
 }
 
 void setup()
 {
     Wire.begin();
     Serial.begin(115200);
+    
+    initializeMPU(mpu7, 0, 7);
+    initializeMPU(mpu6, 1, 6);
+    initializeMPU(mpu5, 2, 5);
+    initializeMPU(mpu4, 3, 4);
+}
 
-    TCA9548A(7);
-    mpu7.initialize();
-    devStatus = mpu7.dmpInitialize();
-    mpu7.setXGyroOffset(220);
-    mpu7.setYGyroOffset(76);
-    mpu7.setZGyroOffset(-85);
-    mpu7.setZAccelOffset(1788);
-    if (devStatus == 0)
+void readMPU(MPU6050 &mpu, uint8_t index, uint8_t channel)
+{
+    TCA9548A(channel);
+    if (!dmpReady[index])
+        return;
+
+    mpuIntStatus[index] = mpu.getIntStatus();
+    fifoCount[index] = mpu.getFIFOCount();
+
+    if ((mpuIntStatus[index] & 0x10) || fifoCount[index] == 1024)
     {
-        Serial.println("foda");
-        mpu7.setDMPEnabled(true);
-
-        mpuIntStatus = mpu7.getIntStatus();
-        dmpReady = true;
-        packetSize = mpu7.dmpGetFIFOPacketSize();
+        mpu.resetFIFO();
     }
-    delay(100);
-
-    TCA9548A(6);
-    delay(100);
-    mpu6.initialize();
-    devStatus6 = mpu6.dmpInitialize();
-    mpu6.setXGyroOffset(220);
-    mpu6.setYGyroOffset(76);
-    mpu6.setZGyroOffset(-85);
-    mpu6.setZAccelOffset(1788);
-    if (devStatus6 == 0)
+    else if (mpuIntStatus[index] & 0x02)
     {
-        Serial.println("foda 6");
-        mpu6.setDMPEnabled(true);
-        mpuIntStatus6 = mpu6.getIntStatus();
-        dmpReady6 = true;
-        packetSize6 = mpu6.dmpGetFIFOPacketSize();
-    }
+        while (fifoCount[index] < packetSize[index])
+        {
+            fifoCount[index] = mpu.getFIFOCount();
+        }
+        mpuInterrupt[index] = false;
+        mpuIntStatus[index] = mpu.getIntStatus();
+        fifoCount[index] = mpu.getFIFOCount();
 
+        mpu.getFIFOBytes(fifoBuffer[index], packetSize[index]);
+        fifoCount[index] -= packetSize[index];
+        mpu.dmpGetQuaternion(&q[index], fifoBuffer[index]);
+        mpu.dmpGetGravity(&gravity[index], &q[index]);
+        mpu.dmpGetYawPitchRoll(ypr[index], &q[index], &gravity[index]);
+        delay(1);
+    }
 }
 
 void loop()
 {
-    float yaw1, pitch1, roll1, yaw2, pitch2, roll2;
+    float yaw[4], pitch[4], roll[4];
 
-    TCA9548A(7);
-    if (!dmpReady)
-        return;
+    readMPU(mpu7, 0, 7);
+    readMPU(mpu6, 1, 6);
+    readMPU(mpu5, 2, 5);
+    readMPU(mpu4, 3, 4);
 
-    // Verifica o status da interrupção
-    mpuIntStatus = mpu7.getIntStatus();
-    fifoCount = mpu7.getFIFOCount();
-
-    if ((mpuIntStatus & 0x10) || fifoCount == 1024)
+    for (int i = 0; i < 4; i++)
     {
-        mpu7.resetFIFO();
-    }
-    else if (mpuIntStatus & 0x02)
-    {
-        while (fifoCount < packetSize)
-        {
-            fifoCount = mpu7.getFIFOCount();
-        }
-        mpuInterrupt = false; // Reseta a interrupção
-        mpuIntStatus = mpu7.getIntStatus();
-        fifoCount = mpu7.getFIFOCount();
-
-        mpu7.getFIFOBytes(fifoBuffer, packetSize);
-        fifoCount -= packetSize;
-        mpu7.dmpGetQuaternion(&q, fifoBuffer);
-        mpu7.dmpGetGravity(&gravity, &q);
-        mpu7.dmpGetYawPitchRoll(ypr, &q, &gravity);
-
-        yaw1 = ypr[0] * 180 / M_PI;
-
-        pitch1 = ypr[1] * 180 / M_PI;
-
-        roll1 = ypr[2] * 180 / M_PI;
-        delay(20); // Aguarda 800 ms antes da próxima leitura
+        yaw[i] = ypr[i][0] * 180 / M_PI;
+        pitch[i] = ypr[i][1] * 180 / M_PI;
+        roll[i] = ypr[i][2] * 180 / M_PI;
     }
 
-    TCA9548A(6);
-    if (!dmpReady6)
-        return;
+    Serial.print(yaw[0]); Serial.print("/");
+    Serial.print(pitch[0]); Serial.print("/");
+    Serial.print(roll[0]); Serial.print("/");
+    Serial.print(yaw[1]); Serial.print("/");
+    Serial.print(pitch[1]); Serial.print("/");
+    Serial.print(roll[1]); Serial.print("/");
+    Serial.print(yaw[2]); Serial.print("/");
+    Serial.print(pitch[2]); Serial.print("/");
+    Serial.print(roll[2]); Serial.print("/");
+    Serial.print(yaw[3]); Serial.print("/");
+    Serial.print(pitch[3]); Serial.print("/");
+    Serial.println(roll[3]);
 
-    mpuIntStatus6 = mpu6.getIntStatus();
-    fifoCount6 = mpu6.getFIFOCount();
-
-    if ((mpuIntStatus6 & 0x10) || fifoCount6 == 1024)
-    {
-        mpu6.resetFIFO();
-    }
-    else if (mpuIntStatus6 & 0x02)
-    {
-        while (fifoCount6 < packetSize6)
-        {
-            fifoCount6 = mpu6.getFIFOCount();
-        }
-        mpuInterrupt6 = false; // Reseta a interrupção
-        mpuIntStatus6 = mpu6.getIntStatus();
-        fifoCount6 = mpu6.getFIFOCount();
-
-        mpu6.getFIFOBytes(fifoBuffer6, packetSize6);
-        fifoCount6 -= packetSize6;
-        mpu6.dmpGetQuaternion(&q6, fifoBuffer6);
-        mpu6.dmpGetGravity(&gravity6, &q6);
-        mpu6.dmpGetYawPitchRoll(ypr6, &q6, &gravity6);
-
-        // Serial.print("Yaw6:");
-        yaw2 = ypr6[0] * 180 / M_PI;
-
-        pitch2 = ypr6[1] * 180 / M_PI;
-
-        roll2 = ypr6[2] * 180 / M_PI;
-        // Serial.println("");
-        delay(20);
-    }
-
-    Serial.print(yaw1);
-    Serial.print("/");
-    Serial.print(pitch1);
-    Serial.print("/");
-    Serial.print(roll1);
-    Serial.print("/");
-    Serial.print(yaw2);
-    Serial.print("/");
-    Serial.print(pitch2);
-    Serial.print("/");
-    Serial.println(roll2);
 }
